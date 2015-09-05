4e3c147ff7ca80e1dabc2757e0fe21f123ece689
nguyen thanh tan <nguyenthanhtan@nguyen-thanh-tans-MacBook-Pro.local>
2015-09-05 14:22:40 +0700

    - Say hello world

--- RadioLoveEnglish/ViewController.m
+++ RadioLoveEnglish/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // Write the first line code
+    NSLog(@"Hello World!");
 }
 
 - (void)didReceiveMemoryWarning {
